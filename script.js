a209e37580849b80ab977397c68448e3d0d3d306
Eduzz4 <eduardo.s.luciano1@gmail.com>
2025-06-20 14:24:26 -0300

    Implementa lógica de vitória com condicionais
    -função playRound compara jogadas
    -atualiza o placar e exibe o resultado

--- script.js
+++ script.js
@@ -3,25 +3,43 @@ console.log("Esse jogo terá 5 rodadas com sistema de pontuação");
 console.log("Para começar digite: playGame()");
 
 function playGame() {
-function getComputerChoice() {
-  let choice = Math.floor(Math.random() * 3);
-  switch (choice) {
-    case 0:
-      return "pedra";
-    case 1:
-      return "papel";
-    case 2:
-      return "tesoura";
+  function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    switch (choice) {
+      case 0:
+        return "pedra";
+      case 1:
+        return "papel";
+      case 2:
+        return "tesoura";
+    }
   }
-}
 
-function getHumanChoice() {
+  function getHumanChoice() {
     let choice = prompt("pedra, papel ou tesoura?");
     return choice;
-}
+  }
 
-let humanChoice = getHumanChoice();
-console.log(getComputerChoice());
+  function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+      console.log("Empate!");
+    } else if (
+      (humanChoice === "pedra" && computerChoice === "tesoura") ||
+      (humanChoice === "papel" && computerChoice === "pedra") ||
+      (humanChoice === "tesoura" && computerChoice === "papel")
+    ) {
+      console.log("você ganhou!");
+      humanScore++;
+    } else {
+      console.log("você perdeu!");
+      computerScore++;
+    }
+  }
+
+  let humanChoice = getHumanChoice();
+  let computerChoice = getComputerChoice();
+  console.log(computerChoice);
+  playRound(humanChoice, computerChoice);
 }
 let humanScore = 0;
 let computerScore = 0;
