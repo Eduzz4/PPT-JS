4e03cacac0f1be11482b1dc35a9f2ef3738aa462
Eduardo <eduardo.s.luciano1@gmail.com>
2026-05-02 17:59:42 -0300

    refactor: reorganiza funções do jogo para melhor legibilidade

--- script.js
+++ script.js
@@ -3,52 +3,51 @@ console.log("Esse jogo terá 5 rodadas com sistema de pontuação");
 console.log("Para acessar a pontuação, digite: checkScore()");
 console.log("Para começar digite: playGame()");
 
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * 3);
+  switch (choice) {
+    case 0:
+      return "pedra";
+    case 1:
+      return "papel";
+    case 2:
+      return "tesoura";
+  }
+}
+
+function getHumanChoice() {
+  let choice = prompt("pedra, papel ou tesoura?");
+  return choice;
+}
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    return "Empate!";
+  } else if (
+    (humanChoice === "pedra" && computerChoice === "tesoura") ||
+    (humanChoice === "papel" && computerChoice === "pedra") ||
+    (humanChoice === "tesoura" && computerChoice === "papel")
+  ) {
+    return "você ganhou!";
+    humanScore++;
+  } else {
+    return "você perdeu!";
+    computerScore++;
+  }
+}
+
 function playGame() {
-  function getComputerChoice() {
-    let choice = Math.floor(Math.random() * 3);
-    switch (choice) {
-      case 0:
-        return "pedra";
-      case 1:
-        return "papel";
-      case 2:
-        return "tesoura";
-    }
-  }
-
-  function getHumanChoice() {
-    let choice = prompt("pedra, papel ou tesoura?");
-    return choice;
-  }
-
-  function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-      console.log("Empate!");
-    } else if (
-      (humanChoice === "pedra" && computerChoice === "tesoura") ||
-      (humanChoice === "papel" && computerChoice === "pedra") ||
-      (humanChoice === "tesoura" && computerChoice === "papel")
-    ) {
-      console.log("você ganhou!");
-      humanScore++;
-    } else {
-      console.log("você perdeu!");
-      computerScore++;
-    }
-  }
-
   let humanChoice = getHumanChoice();
   let computerChoice = getComputerChoice();
   console.log(`Você escolheu: ${humanChoice}`);
   console.log(`Computador escolheu: ${computerChoice}`);
 
-  playRound(humanChoice, computerChoice);
-  return "Rodada concluída!";
+  return playRound(humanChoice, computerChoice);
 }
 
-  function checkScore() {
-    return `Placar: Você ${humanScore} - ${computerScore} Computador`;
-  }
+function checkScore() {
+  return `Placar: Você ${humanScore} - ${computerScore} Computador`;
+}
 
 let humanScore = 0;
 let computerScore = 0;
